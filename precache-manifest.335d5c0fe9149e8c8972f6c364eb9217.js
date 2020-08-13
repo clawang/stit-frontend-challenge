2f0985ccae0f819507f1a8a5ceb0408c99e394fc
Claire Wang <clairewang@Claires-MacBook-Pro-10.local>
2020-08-13 17:18:56 -0400

    Updates

---
+++ precache-manifest.335d5c0fe9149e8c8972f6c364eb9217.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193a6065a34b6814218a9e782d099724",
+    "url": "/stit-frontend-challenge/index.html"
+  },
+  {
+    "revision": "0525b75649cd23b6c648",
+    "url": "/stit-frontend-challenge/static/css/main.8bf9a710.chunk.css"
+  },
+  {
+    "revision": "a2a0bac1b686bf42a423",
+    "url": "/stit-frontend-challenge/static/js/2.d55948b6.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/stit-frontend-challenge/static/js/2.d55948b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0525b75649cd23b6c648",
+    "url": "/stit-frontend-challenge/static/js/main.b63287f6.chunk.js"
+  },
+  {
+    "revision": "20a4eba6eeef96d64df2",
+    "url": "/stit-frontend-challenge/static/js/runtime-main.eb112289.js"
+  },
+  {
+    "revision": "a2373d8bf821fc3341212d14083e961e",
+    "url": "/stit-frontend-challenge/static/media/cafe.a2373d8b.jpg"
+  },
+  {
+    "revision": "74f8e421671ee239df3d450839c1299b",
+    "url": "/stit-frontend-challenge/static/media/halfstar.74f8e421.svg"
+  },
+  {
+    "revision": "f00d58bd90ce4a91312d55bf21cc5703",
+    "url": "/stit-frontend-challenge/static/media/marker.f00d58bd.png"
+  },
+  {
+    "revision": "a07280f4198e526afd8fed6c0166ba80",
+    "url": "/stit-frontend-challenge/static/media/pin.a07280f4.png"
+  },
+  {
+    "revision": "967d62c7d17bec1b31ac67c806c708e6",
+    "url": "/stit-frontend-challenge/static/media/star.967d62c7.svg"
+  },
+  {
+    "revision": "77ac57be7c8233766c369cb32db2b4e8",
+    "url": "/stit-frontend-challenge/static/media/unfilled.77ac57be.svg"
+  }
+]);
